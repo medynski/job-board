5ed8d3a0ae9a2ce5689e81d15e551d604d4f0121
medynski <wmedynski@gmail.com>
2024-01-30 15:53:15 +0100

    Fixed issue with wildcard and static files in fastify server

--- apps/server-api/src/app/plugins/static.ts
+++ apps/server-api/src/app/plugins/static.ts
@@ -7,5 +7,6 @@ export default fp(async function (fastify: FastifyInstance) {
   // serve static app
   fastify.register(fastifyStatic, {
     root: path.resolve(path.dirname(''), './dist/apps/client-react'),
+    wildcard: false, // set this to false to disable wildcard for static routes
   });
 });
--- apps/server-api/src/app/routes/root.ts
+++ apps/server-api/src/app/routes/root.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify';
 import * as path from 'path';
 
 export default async function (fastify: FastifyInstance) {
-  fastify.get('/', async (req, reply) => {
+  fastify.get('/*', async (req, reply) => {
     return reply.sendFile(
       'index.html',
       path.resolve(path.dirname(''), './dist/apps/client-react')
