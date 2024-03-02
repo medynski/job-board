07595e0a41a2aebb4d62e6bd22feff432aa3b7dd
medynski <wmedynski@gmail.com>
2024-03-02 09:29:17 +0100

    Fixed issue with user permission status after logout

--- apps/client-react/src/app/hooks/queries/usePermissions.ts
+++ apps/client-react/src/app/hooks/queries/usePermissions.ts
@@ -7,7 +7,7 @@ export const usePermissionsQuery = () => {
   const userId = useAuthStore((state) => state.user?.userId);
   const permissionsQuery = useQuery({
     enabled: !!userId,
-    queryKey: ['permissions'],
+    queryKey: ['permissions', userId],
     queryFn: () =>
       axios.get(`${apiUrl()}/permission/${userId}`).then((res) => res.data),
   });
